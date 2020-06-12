ef39441186b344dc2f662279b024ac75ab17ad82
richard.ellis <info@lookstechnical.co.uk>
2020-06-12 19:38:11 +0100

    tweaks

--- src/components/Stickyheader.js
+++ src/components/Stickyheader.js
@@ -1,36 +1,41 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react"
 
 const StickyHeader = () => {
-	const [ isSticky, setSticky ] = useState(false);
-	const ref = useRef(null);
-	const handleScroll = () => {
-		if (ref.current && ref.current.getBoundingClientRect().top <= 0) {
-			setSticky(true);
-		} else {
-			setSticky(false);
-		}
-	};
+    const [isSticky, setSticky] = useState(false)
+    const ref = useRef(null)
+    const handleScroll = () => {
+        if (ref.current && ref.current.getBoundingClientRect().top <= 0) {
+            setSticky(true)
+        } else {
+            setSticky(false)
+        }
+    }
 
-	useEffect(() => {
-		window.addEventListener('scroll', handleScroll);
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
 
-		return () => {
-			window.removeEventListener('scroll', () => handleScroll);
-		};
-	}, []);
+        return () => {
+            window.removeEventListener("scroll", () => handleScroll)
+        }
+    }, [])
 
-	return (
-		<div className={`has-background-grey-dark contact-header ${isSticky ? ' sticky' : ''}`} ref={ref}>
-			<div className="container has-text-white">
-				<div>
-					Locked out ?
-				</div>
-				<div>
-					call now <span className="has-text-weight-bold has-text-white">07912 294 111</span>
-				</div>
-			</div>
-		</div>
-	);
-};
+    return (
+        <div
+            className={`has-background-grey-dark contact-header ${
+                isSticky ? " sticky" : ""
+            }`}
+            ref={ref}
+        >
+            <div className="container has-text-white">
+                <div>Locked out ?</div>
+                <div>
+                    <a href="tel:07912 294 111" className="has-text-white">
+                        Call Now: 07912 294 111
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-export default StickyHeader;
+export default StickyHeader
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,117 +1,145 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, graphql } from 'gatsby';
+import React from "react"
+import PropTypes from "prop-types"
+import { Link, graphql } from "gatsby"
 
-import Layout from '../components/Layout';
-import door from '../img/door.jpg';
-import FeaturedServices from '../components/FeaturedServices';
+import Layout from "../components/Layout"
+import door from "../img/door.jpg"
+import FeaturedServices from "../components/FeaturedServices"
 
-export const IndexPageTemplate = ({ image, title, heading, subheading, mainpitch, description, intro }) => (
-	<div>
-		<section
-			className="section"
-			style={{
-				backgroundImage: `url(${door})`,
-				backgroundPosition: 'center center',
-				backgroundSize: 'cover',
-				padding: '180px 0px 180px 0px',
-				transition: 'background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;'
-			}}
-		>
-			<div className="has-text-white" style={{ padding: '50px' }}>
-				<h1 className="is-size-1 has-text-weight-bold">Door and window specialists</h1>
-				<h2 className="is-size-3 has-text-weight-bold">Leeds and Bradford</h2>
-			</div>
-		</section>
-		<section className="section  has-text-centered">
-			<header className="mb-l">
-				<p className="is-light-heading is-uppercase is-size-5">Commerical or Residential</p>
-				<h2 className="is-size-2 has-text-weight-bold">What services do we provide?</h2>
-			</header>
-			<FeaturedServices />
-		</section>
-		<section className="section dark areas">
-			<section className="section locksmith columns">
-				<div>
-					<p className="is-light-heading is-uppercase is-size-5">Locked out?</p>
-					<h3 className="is-size-2 has-text-weight-bold">24 hour Locksmith service</h3>
-					<p>Available throughout the Leeds and Bradford.</p>
-					<Link to="/services/24-hour-locksmith/">Find out more</Link>
-				</div>
-				<div>
-					<a href="mailto:07912 294 111" className="button is-large is-danger">
-						Call Now: 07912 294 111
-					</a>
-				</div>
-			</section>
-		</section>
-		<section className="section has-text-centered">
-			<div>
-				<p className="is-light-heading is-uppercase is-size-5">Testimonials</p>
-				<h3 className="is-size-2 has-text-weight-bold">View our customers feedback</h3>
-			</div>
-		</section>
-		<section className="section dark">
-			<div>
-				<h3 className="is-size-2 has-text-weight-bold">Get in touch</h3>
-			</div>
-		</section>
-	</div>
-);
+export const IndexPageTemplate = ({
+    image,
+    title,
+    heading,
+    subheading,
+    mainpitch,
+    description,
+    intro,
+}) => (
+    <div>
+        <section
+            className="section"
+            style={{
+                backgroundImage: `url(${door})`,
+                backgroundPosition: "center center",
+                backgroundSize: "cover",
+                padding: "180px 0px 180px 0px",
+                transition:
+                    "background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;",
+            }}
+        >
+            <div className="has-text-white" style={{ padding: "50px" }}>
+                <h1 className="is-size-1 has-text-weight-bold">
+                    Door and window specialists
+                </h1>
+                <h2 className="is-size-3 has-text-weight-bold">
+                    Leeds and Bradford
+                </h2>
+            </div>
+        </section>
+        <section className="section  has-text-centered">
+            <header className="mb-l">
+                <p className="is-light-heading is-uppercase is-size-5">
+                    Commerical or Residential
+                </p>
+                <h2 className="is-size-2 has-text-weight-bold">
+                    What services do we provide?
+                </h2>
+            </header>
+            <FeaturedServices />
+        </section>
+        <section className="section dark areas">
+            <section className="section locksmith columns">
+                <div>
+                    <p className="is-light-heading is-uppercase is-size-5">
+                        Locked out?
+                    </p>
+                    <h3 className="is-size-2 has-text-weight-bold">
+                        24 hour Locksmith service
+                    </h3>
+                    <p>Available throughout West Yorkshire.</p>
+                    <Link to="/services/24-hour-locksmith/">Find out more</Link>
+                </div>
+                <div>
+                    <a
+                        href="tel:07912 294 111"
+                        className="button is-large is-danger"
+                    >
+                        Call Now: 07912 294 111
+                    </a>
+                </div>
+            </section>
+        </section>
+        <section className="section has-text-centered">
+            <div>
+                <p className="is-light-heading is-uppercase is-size-5">
+                    Testimonials
+                </p>
+                <h3 className="is-size-2 has-text-weight-bold">
+                    View our customers feedback
+                </h3>
+            </div>
+        </section>
+        <section className="section dark">
+            <div>
+                <h3 className="is-size-2 has-text-weight-bold">Get in touch</h3>
+            </div>
+        </section>
+    </div>
+)
 
 IndexPageTemplate.propTypes = {
-	image: PropTypes.oneOfType([ PropTypes.object, PropTypes.string ]),
-	title: PropTypes.string,
-	heading: PropTypes.string,
-	subheading: PropTypes.string,
-	mainpitch: PropTypes.object,
-	description: PropTypes.string,
-	intro: PropTypes.shape({
-		blurbs: PropTypes.array
-	})
-};
+    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    title: PropTypes.string,
+    heading: PropTypes.string,
+    subheading: PropTypes.string,
+    mainpitch: PropTypes.object,
+    description: PropTypes.string,
+    intro: PropTypes.shape({
+        blurbs: PropTypes.array,
+    }),
+}
 
 const IndexPage = ({ data }) => {
-	const { frontmatter } = data.markdownRemark;
+    const { frontmatter } = data.markdownRemark
 
-	return (
-		<Layout>
-			<IndexPageTemplate
-				image={frontmatter.image}
-				title={frontmatter.title}
-				heading={frontmatter.heading}
-				subheading={frontmatter.subheading}
-				mainpitch={frontmatter.mainpitch}
-				description={frontmatter.description}
-				intro={frontmatter.intro}
-			/>
-		</Layout>
-	);
-};
+    return (
+        <Layout>
+            <IndexPageTemplate
+                image={frontmatter.image}
+                title={frontmatter.title}
+                heading={frontmatter.heading}
+                subheading={frontmatter.subheading}
+                mainpitch={frontmatter.mainpitch}
+                description={frontmatter.description}
+                intro={frontmatter.intro}
+            />
+        </Layout>
+    )
+}
 
 IndexPage.propTypes = {
-	data: PropTypes.shape({
-		markdownRemark: PropTypes.shape({
-			frontmatter: PropTypes.object
-		})
-	})
-};
+    data: PropTypes.shape({
+        markdownRemark: PropTypes.shape({
+            frontmatter: PropTypes.object,
+        }),
+    }),
+}
 
-export default IndexPage;
+export default IndexPage
 
 export const pageQuery = graphql`
-	query IndexPageTemplate {
-		markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-			frontmatter {
-				title
-				heading
-				subheading
-				mainpitch {
-					title
-					description
-				}
-				description
-			}
-		}
-	}
-`;
+    query IndexPageTemplate {
+        markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+            frontmatter {
+                title
+                heading
+                subheading
+                mainpitch {
+                    title
+                    description
+                }
+                description
+            }
+        }
+    }
+`
